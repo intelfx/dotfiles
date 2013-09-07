996ad48f554347fede592757084fd5320121c8c3
Ivan Shapovalov <intelfx100@gmail.com>
2013-09-07 23:15:10 +0400

    rc.lua: improve netctl-auto-ng controls.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -298,17 +298,18 @@ vicious.register (netinfo, vicious.widgets.wifi,
 	end, 37, "wlan0")
 
 netmenu = awful.menu ({ items = {
-	{ "Refresh status",                function () vicious.force ({ netinfo }) end },
-	{ "Set mode [running-continuous]", function () awful.util.spawn (exec_and_update_term_hold ("sudo netctl-auto-ng continuous", "netinfo")) end },
-	{ "Set mode [running-once]",       function () awful.util.spawn (exec_and_update_term_hold ("sudo netctl-auto-ng once", "netinfo")) end },
+	{ "Launch wifi-menu",              function () awful.util.spawn (exec_and_update_term_hold ("sudo wifi-menu", "netinfo")) end },
+	{ "Set mode [running]",            function () awful.util.spawn (exec_and_update_term_hold ("sudo netctl-auto-ng bootup", "netinfo")) end },
+	{ "Set mode [resume]",             function () awful.util.spawn (exec_and_update_term_hold ("sudo netctl-auto-ng resume", "netinfo")) end },
+	{ "Set mode [suspended]",          function () awful.util.spawn (exec_and_update_term_hold ("sudo netctl-auto-ng suspend", "netinfo")) end },
 	{ "Set mode [halted]",             function () awful.util.spawn (exec_and_update_term_hold ("sudo netctl-auto-ng halt", "netinfo")) end },
-	{ "Set mode [halted-suspend]",     function () awful.util.spawn (exec_and_update_term_hold ("sudo netctl-auto-ng suspend", "netinfo")) end },
-	{ "Set mode [resume]",             function () awful.util.spawn (exec_and_update_term_hold ("sudo netctl-auto-ng resume", "netinfo")) end }
+	{ "Set mode [running:continuous]", function () awful.util.spawn (exec_and_update_term_hold ("sudo netctl-auto-ng continuous", "netinfo")) end },
+	{ "Set mode [running:once]",       function () awful.util.spawn (exec_and_update_term_hold ("sudo netctl-auto-ng once", "netinfo")) end }
 }})
 
 netinfo:buttons (awful.util.table.join (
 	awful.button ({ }, 1, function () netmenu:toggle () end),
-	awful.button ({ }, 3, function () awful.util.spawn (exec_and_update_term_hold ("sudo netctl-auto-ng halt; sudo wifi-menu", "netinfo")) end)
+	awful.button ({ }, 3, function () vicious.force ({ netinfo }) end)
 ))
 
 
