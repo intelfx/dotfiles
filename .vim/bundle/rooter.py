ee88b458abd289da90fbc2baabb1a3abf7d9b4ad
Ivan Shapovalov <intelfx@intelfx.name>
2017-05-09 01:05:47 +0300

    .vim/bundle/rooter.py: reimplement airblade/vim-rooter and embear/vim-localvimrc in python.

---
+++ .vim/bundle/rooter.py
@@ -0,0 +1,120 @@
+from os import path as p
+from functools import partial
+
+import vim
+import snake
+from snake.plugins.common import *
+
+class Rooter:
+	def __init__(self, path):
+		self.path = path
+
+		# update project root
+		self.root = None
+		path = self.path
+		while True:
+			if not p.exists(p.join(path, ".rooter-ignore")):
+				if p.exists(p.join(path, ".git")):
+					self.root = path
+					break
+
+				if p.isfile(p.join(path, ".pvimrc")):
+					self.root = path
+					break
+
+				if p.isfile(p.join(path, ".pvimrc.py")):
+					self.root = path
+					break
+
+			path, oldpath = p.dirname(path), path
+			if path == oldpath:
+				break
+
+		# update vimrc list
+		self.rcfiles = []
+		path = self.path
+		while True:
+			# we are walking pathes from most specific to least specific,
+			# so process rc files in order of decreasing priority
+			# and reverse the list afterwards.
+			self.rcfiles += self.update_process_vimrc_py(p.join(path, ".lvimrc.py"))
+			self.rcfiles += self.update_process_vimrc(p.join(path, ".lvimrc"))
+			self.rcfiles += self.update_process_vimrc_py(p.join(path, ".pvimrc.py"))
+			self.rcfiles += self.update_process_vimrc(p.join(path, ".pvimrc"))
+
+			path, oldpath = p.dirname(path), path
+			if path == oldpath:
+				break
+
+		self.rcfiles.reverse()
+
+	def update_process_vimrc(self, vimrc_path):
+		if p.isfile(vimrc_path):
+			cmd = f"source '{snake.escape_string_sq(vimrc_path)}'"
+			return [ partial(vim.command, cmd) ]
+		return []
+
+	def update_process_vimrc_py(self, vimrc_py_path):
+		if p.isfile(vimrc_py_path):
+			spec = importlib.util.spec_from_file("", vimrc_path)
+			module = importlib.util.module_from_spec(spec)
+			return [ partial(spec.loader.exec_module, module) ]
+		return []
+
+	def apply(self, cd = True, vimrc = True):
+		# apply project root
+		if cd and self.root is not None:
+			vim.command(f"lcd {escape_filename(self.root)}")
+
+		# apply lvimrc files
+		if vimrc:
+			for rc in self.rcfiles:
+				rc()
+
+rooter_objs = {}
+
+@snake.on_autocmd("BufNew,BufFilePost,BufWinEnter", "*")
+def rooter(ctx):
+	# due to the completely horrible and brain-dead way Vim emits autocmds,
+	# we will catch all autocmd events with a single handler and deduce our
+	# way disregarding the exact event that happened.
+	#
+	# TODO: when Vim is opened without arguments in a non-root directory and
+	#       the empty buffer is saved under a certain name, Vim is re-rooted
+	#       before the save occurs and the file is saved in the root instead
+	#       of what user wants.
+
+	b = vim.current.buffer.number
+
+	path = p.dirname(snake.expand("%:p"))
+	cd = True
+	if path == "":
+		path = vim.eval("getcwd()")
+		cd = False
+
+	try:
+		obj = rooter_objs[b]
+		if obj.path != path:
+			raise KeyError # inject
+	except KeyError:
+		obj = Rooter(path)
+		rooter_objs[b] = obj
+
+	#print(f"Log {incctr()}: applying rooter with {obj.path} -> {obj.root} for buffer {b} (cd: {cd})")
+	obj.apply(cd = cd)
+
+@snake.on_autocmd("BufDelete", "*")
+def rooter_delete(ctx):
+	b = snake.expand("<abuf>")
+
+	try:
+		#obj = rooter_objs[b]
+		#print(f"Log {incctr()}: dropping rooter with {obj.path} -> {obj.root} for buffer {b}")
+		del rooter_objs[b]
+	except KeyError:
+		pass
+
+
+#  vim: set ft=python ts=8 sw=8 tw=0 noet :
+
+
