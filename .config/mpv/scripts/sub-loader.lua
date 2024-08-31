2c2c97e64224dc740f8e2f94cf16008905a7ad9d
Ivan Shapovalov <intelfx@intelfx.name>
2024-08-31 21:47:35 +0200

    .config/mpv: sub-loader.lua: rework, parse URLs properly

--- .config/mpv/scripts/sub-loader.lua
+++ .config/mpv/scripts/sub-loader.lua
@@ -1,9 +1,18 @@
 local mp = require 'mp'
 
-mp.add_hook('on_load', 10, function()
-	local path = mp.get_property('path')
+package.path = mp.command_native({"expand-path", "~~/script-libs/?.lua;"})..package.path
+local inspect = require 'inspect'
+local url = require 'net/url'
+local lib = require 'lib'
 
-	if path:find('://') then
-		mp.commandv('change-list', 'sub-files', 'append', (path:gsub('[^.]+$', 'srt')))
+mp.add_hook('on_load', 10, function()
+	local pat_ext = '%.[^/.]+$'
+
+	local path = mp.get_property('path')
+	local parsed = url.parse(path)
+
+	if parsed.authority ~= '' and parsed.path:match(pat_ext) then
+		parsed.path = parsed.path:gsub(pat_ext, '.srt')
+		mp.commandv('change-list', 'sub-files', 'append', tostring(parsed))
 	end
 end)
